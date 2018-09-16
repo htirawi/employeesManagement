98433d0798aed75008fc94ec686d4d90b8d911c0
htirawi <hmtirawi@gmail.com>
2018-09-17 02:01:13 +0300

    add components to angular module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquipmentComponent,
+    EmployeesComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
